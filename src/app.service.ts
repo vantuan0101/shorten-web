54e6210ff60b018f1bec2e0a5bfad363177e89cb
Văn Tuấn <82825666+vantuan0101@users.noreply.github.com>
2022-12-08 13:41:44 +0700

    up

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,15 +1,15 @@
-import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Response, Request } from 'express';
-import { Model } from 'mongoose';
-import { InjectRedis, Redis } from '@nestjs-modules/ioredis';
-import { handlePageOptions } from './common/handlePageOptions';
-import { PageOptionsDto } from './shorten-link/dto/PageOptionsDto';
-import { User, UserDocument } from './users/entites/user.entites';
+import { Injectable, HttpException, HttpStatus } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { Response, Request } from "express";
+import { Model } from "mongoose";
+import { InjectRedis, Redis } from "@nestjs-modules/ioredis";
+import { handlePageOptions } from "./common/handlePageOptions";
+import { PageOptionsDto } from "./shorten-link/dto/PageOptionsDto";
+import { User, UserDocument } from "./users/entites/user.entites";
 import {
   ShortenLink,
   ShortenLinkDocument,
-} from './shorten-link/entities/shorten-link.entity';
+} from "./shorten-link/entities/shorten-link.entity";
 
 @Injectable()
 export class AppService {
@@ -17,7 +17,7 @@ export class AppService {
     @InjectModel(ShortenLink.name)
     private shortenLinkService: Model<ShortenLinkDocument>,
     @InjectModel(User.name) private userService: Model<UserDocument>,
-    @InjectRedis() private readonly redis: Redis,
+    @InjectRedis() private readonly redis: Redis
   ) {}
 
   async redirectLink(shortedUrl: string, response: Response, request: Request) {
@@ -26,10 +26,10 @@ export class AppService {
       {
         shortLink: urlShortLink,
       },
-      { $inc: { countClick: 1 } },
+      { $inc: { countClick: 1 } }
     );
     if (!shortenLinkResults) {
-      throw new HttpException('Link Not Found', HttpStatus.BAD_REQUEST);
+      throw new HttpException("Link Not Found", HttpStatus.BAD_REQUEST);
     }
 
     const { user } = request.cookies;
@@ -37,7 +37,7 @@ export class AppService {
     if (user) {
       await this.userService.findOneAndUpdate(
         { _id: user._id },
-        { $push: { clickedLink: shortenLinkResults._id } },
+        { $push: { clickedLink: shortenLinkResults._id } }
       );
     }
     return response.redirect(shortenLinkResults?.linkToRedirect);
@@ -47,19 +47,21 @@ export class AppService {
     const { user } = request.cookies;
     if (!user) {
       return {
+        disable: false,
         status: HttpStatus.UNAUTHORIZED,
-        message: 'You are not yet login',
+        message: "You are not yet login",
       };
       // throw new HttpException('You are not yet login', HttpStatus.BAD_REQUEST);
     }
     const checkIp = await this.redis.hget(
       `user:${user._id}:ip`,
-      `${ipAddress}`,
+      `${ipAddress}`
     );
     if (!checkIp) {
       return {
+        disable: true,
         status: HttpStatus.UNAUTHORIZED,
-        message: 'User has been disabled , please login again',
+        message: "User has been disabled , please login again",
       };
     }
     return checkIp;
